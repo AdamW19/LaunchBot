67c5486a632509d15c203c96c82bcd860165309b
Adam Wang <adam.kuang@yahoo.com>
2020-12-20 14:35:39 -0800

    started base code for rotation information

---
+++ modules/execute.py
@@ -0,0 +1,21 @@
+# From Splatbot: https://github.com/ktraw2/SplatBot/blob/master/modules/execute.py
+
+import asyncio
+
+
+async def run_command(*args):
+    # Create subprocess
+    process = await asyncio.create_subprocess_exec(
+        *args,
+        # stdout must a pipe to be accessible as process.stdout
+        stdout=asyncio.subprocess.PIPE)
+    # Wait for the subprocess to finish
+    stdout, stderr = await process.communicate()
+    # Return stdout
+    return stdout.decode().strip()
+
+
+async def render_gif(image_base: str, gif_filename: str):
+    await run_command('ffmpeg', '-framerate', '0.33', '-f', 'image2', '-i', image_base + '-%d.png', '-filter_complex',
+                      '[0:v] split [a][b];[a] palettegen=stats_mode=single [p]; [b][p] paletteuse=dither=none',
+                      '-loglevel', 'warning', '-y', gif_filename)
---
+++ modules/splatnet.py
@@ -0,0 +1,27 @@
+# From Splatbot: https://github.com/ktraw2/SplatBot/blob/master/modules/splatnet.py
+
+from modules.async_client import AsyncClient
+
+
+class Splatnet(AsyncClient):
+    def __init__(self, user_agent="", request_prefix="", request_suffix="", session=None):
+        super(Splatnet, self).__init__(user_agent=user_agent, request_prefix=request_prefix,
+                                       request_suffix=request_suffix, session=session)
+
+    async def get_turf(self):
+        return (await self.send_json_request("schedules"))['regular']
+
+    async def get_ranked(self):
+        return (await self.send_json_request("schedules"))['gachi']
+
+    async def get_league(self):
+        return (await self.send_json_request("schedules"))['league']
+
+    async def get_salmon_detail(self):
+        return (await self.send_json_request("coop-schedules"))['details']
+
+    async def get_salmon_schedule(self):
+        return (await self.send_json_request("coop-schedules"))['schedules']
+
+    async def get_na_splatfest(self):
+        return (await self.send_json_request("festivals"))['na']
