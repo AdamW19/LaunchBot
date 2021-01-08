5dc4dc531b0bab79d6183959a5d8b19610c91f33
Adam Wang <adam.kuang@yahoo.com>
2021-01-08 15:17:18 -0800

    commented out code that was doing not a good thing

--- img/stages/test.py
+++ img/stages/test.py
@@ -1,15 +1,15 @@
-import glob
-import os
+# import glob
+# import os
 
 # what this did is to rename all of the stage images from splatoonwiki to something we want
 
 FILE_PREFIX = "./img/stages/"
-list_of_files = glob.glob(FILE_PREFIX + "*.png")
+# list_of_files = glob.glob(FILE_PREFIX + "*.png")
 
-for file in list_of_files:
-    new_filename = file[15:]  # removes "S2-Stage-"
-    new_filename = new_filename.replace("_", "-")
-    new_filename = FILE_PREFIX + new_filename
-    os.rename(file, new_filename)
+# for file in list_of_files:
+    # new_filename = file[15:]  # removes "S2-Stage-"
+    # new_filename = new_filename.replace("_", "-")
+    # new_filename = FILE_PREFIX + new_filename
+    # os.rename(file, new_filename)
 
-print("Done")
+# print("Done")
